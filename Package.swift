e998ce2bca497623b06013de0d4f5f276074d44b
DongJun96 <djlee1201@naver.com>
2024-09-03 01:42:46 +0900

    fix: target dependencies 수정

--- Package.swift
+++ Package.swift
@@ -1,12 +1,8 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "NaverMap-SPM-iOS",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NaverMap-SPM-iOS",
             targets: ["NaverMap-SPM-iOS"]),
@@ -21,9 +17,8 @@ let package = Package(
             path: "./Frameworks/NMapsGeometry.xcframework"
         ),
         .target(
-            name: "NaverMap-SPM-iOS"),
-        .testTarget(
-            name: "NaverMap-SPM-iOSTests",
-            dependencies: ["NaverMap-SPM-iOS"]),
+            name: "NaverMap-SPM-iOS",
+            dependencies: ["NMapsMap", "NMapsGeometry"]
+        )
     ]
 )
